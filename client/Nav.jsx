a698b44bd5511aae464556a434c428c6e439be6d
Russell Moore <russellwmoore@gmail.com>
2020-09-23 15:51:28 -0400

    Make responsive nav

--- client/Nav.jsx
+++ client/Nav.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export const Navigation = (props) => {
   return (
-    <Nav activeKey="/home">
-      <Nav.Item>
-        <Nav.Link href="/home">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/squares">Squares</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/graphs">Graphs</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/pie">Pie</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/piezoom">piezoom</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar
+      collapseOnSelect
+      bg="primary"
+      variant="dark"
+      expand="lg"
+      fixed="top"
+    >
+      <Navbar.Brand href="/">NYPD-CCRB Visualized</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/squares">Squares</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/graphs">Graphs</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/pie">Pie</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/piezoom">piezoom</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     // <div className="nav">
 
     //   <ul>
